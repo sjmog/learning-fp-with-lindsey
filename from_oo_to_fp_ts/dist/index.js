ec1fb0ca606d1b482d54c3b916ebd9f1245cae33
lindsey <lindsey@makers.tech>
2020-04-22 17:17:03 +0100

    Implement computerWeapon and winner method

--- from_oo_to_fp_ts/dist/index.js
+++ from_oo_to_fp_ts/dist/index.js
@@ -1,7 +1,18 @@
 "use strict";
-function winner() {
+function winner(weapon1, weapon2) {
+    const WIN_CONDITIONS = {
+        'rock': 'scissors',
+        'scissors': 'paper',
+        'paper': 'rock'
+    };
+    if (weapon1 === weapon2) {
+        return 'Everyone\'s a loser!';
+    }
+    const winner = WIN_CONDITIONS[weapon1] === weapon2 ? `${weapon1} wins!` : `${weapon2} wins!`;
+    return winner[0].toUpperCase() + winner.slice(1, -1) + "!";
 }
-function computerWeapon() {
+function computerWeapon(randomisationFunction) {
+    return ['rock', 'scissors', 'paper'][Math.floor(randomisationFunction() * 3)];
 }
 function run(a, b, c) {
 }
--- from_oo_to_fp_ts/index.ts
+++ from_oo_to_fp_ts/index.ts
@@ -1,10 +1,24 @@
 
-function winner() {
+
+function winner(weapon1: string, weapon2: string): string {
+  const WIN_CONDITIONS: {[weapon:string]:string}  = {
+    'rock': 'scissors',
+    'scissors': 'paper',
+    'paper': 'rock'
+  }
+
+  if(weapon1 === weapon2) {
+    return 'Everyone\'s a loser!';
+  }
+
+  const winner = WIN_CONDITIONS[weapon1] === weapon2 ? `${weapon1} wins!` : `${weapon2} wins!`
+
+  return winner[0].toUpperCase() + winner.slice(1, -1) + "!"
 
 }
 
-function computerWeapon() {
-
+function computerWeapon(randomisationFunction: () => number) {
+  return ['rock', 'scissors', 'paper'][Math.floor(randomisationFunction() * 3)]
 }
 
 function run(a:any,b:any,c:any) {
